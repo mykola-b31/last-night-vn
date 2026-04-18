91372cdbb806601fed99bd330cb3e272534b1bc7
Mykola Babko <babkokolya81@gmail.com>
2026-04-18 12:51:57 +0300

    add: settings panel

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -1,20 +1,41 @@
-using CsvHelper.Configuration.Attributes;
 using UnityEngine;
 using UnityEngine.UI;
+using Yarn.Unity;
+using TMPro;
 
 public class GameUIManager : MonoBehaviour
 {
-    [HeaderPrefix("UI Elements")]
+    [Header("UI Elements")]
     public Slider progressSlider;
     public Slider stressSlider;
+
+    [Header("Yarn Spinner UI")]
+    public LinePresenter yarnLinePresenter;
+    public TextMeshProUGUI yarnDialogueText;
 
     private void Start()
     {
         if (progressSlider != null) progressSlider.maxValue = 100;
         if (stressSlider != null) stressSlider.maxValue = 100;
 
+        ApplySettings();
+
         UpdateProgressUI(GameManager.Instance.progress);
         UpdateStressUI(GameManager.Instance.stress);
+    }
+
+    private void ApplySettings()
+    {
+        if (yarnLinePresenter != null)
+        {
+            yarnLinePresenter.lettersPerSecond = PlayerPrefs.GetInt("TextSpeed", 30);
+            yarnLinePresenter.autoAdvance = PlayerPrefs.GetInt("AutoForward", 0) == 1;
+        }
+
+        if (yarnDialogueText != null)
+        {
+            yarnDialogueText.fontSize = PlayerPrefs.GetInt("FontSize", 24);
+        }
     }
 
     private void OnEnable()
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,89 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     public string gameSceneName = "SampleScene";
+
+    [Header("UI Panels")]
+    public GameObject settingsPanel;
+
+    [Header("Audio")]
+    public AudioMixer mainMixer;
+    public Slider musicSlider, sfxSlider;
+
+    [Header("Text & Gameplay")]
+    public Slider textSpeedSlider;
+    public Slider fontSizeSlider;
+    public Toggle autoForwardToggle;
+
+    private void Start()
+    {
+        LoadAudioSetting("MusicVol", musicSlider, "MusicVolume");
+        LoadAudioSetting("SFXVol", sfxSlider, "SFXVolume");
+
+        if (textSpeedSlider != null)
+        {
+            textSpeedSlider.value = PlayerPrefs.GetFloat("TextSpeed", 30f);
+            textSpeedSlider.onValueChanged.AddListener(SetTextSpeed);
+        }
+            
+        if (fontSizeSlider != null)
+        {
+            fontSizeSlider.value = PlayerPrefs.GetInt("FontSize", 24);
+            fontSizeSlider.onValueChanged.AddListener(SetFontSize);
+        }
+
+        if (autoForwardToggle != null)
+        {
+            autoForwardToggle.isOn = PlayerPrefs.GetInt("AutoForward", 0) == 1;
+            autoForwardToggle.onValueChanged.AddListener(SetAutoForward);
+        }
+            
+    }
+
+    private void LoadAudioSetting(string exposedParam, Slider slider, string prefsKey)
+    {
+        float vol = PlayerPrefs.GetFloat(prefsKey, 0.75f);
+        slider.value = vol;
+        SetGroupVolume(exposedParam, vol);
+    }
+
+    public void SetMusicVolume(float value) => SetAudio("MusicVol", "MusicVolume", value);
+    public void SetSFXVolume(float value) => SetAudio("SFXVol", "SFXVolume", value);
+
+    private void SetAudio(string exposedParam, string prefsKey, float value)
+    {
+        SetGroupVolume(exposedParam, value);
+        PlayerPrefs.SetFloat(prefsKey, value);
+    }
+
+    private void SetGroupVolume(string parameter, float value)
+    {
+        float dB = value > 0 ? Mathf.Log10(value) * 20 : -80f;
+        mainMixer.SetFloat(parameter, dB);
+    }
+
+    public void SetTextSpeed(float speed)
+    {
+        PlayerPrefs.SetInt("TextSpeed", Mathf.RoundToInt(speed));
+        PlayerPrefs.Save();
+    }
+
+    public void SetFontSize(float size)
+    {
+        PlayerPrefs.SetInt("FontSize", Mathf.RoundToInt(size));
+        PlayerPrefs.Save();
+    }
+
+    public void SetAutoForward(bool isOn)
+    {
+        PlayerPrefs.SetInt("AutoForward", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
     public void StartNewGame()
     {
         SceneManager.LoadScene(gameSceneName);
@@ -16,8 +96,12 @@ public class MainMenuManager : MonoBehaviour
 
     public void OpenSettings()
     {
-        // TODO: Реалізувати вікно налаштувань
-        Debug.Log("Відкрито налаштування");
+        if (settingsPanel != null) settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        if (settingsPanel != null) settingsPanel.SetActive(false);
     }
 
     public void QuitGame()
